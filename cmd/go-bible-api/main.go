f484e89ee8e096d5eecd26e28600fd761106ae81
Matt Reath <mattreath@icloud.com>
2024-04-03 08:29:21 -0500

    Chi Routing Setup (#1)
    * go mod init
    * chi
    * router and root path handler
    * updated README

---
+++ cmd/go-bible-api/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", Root)
+	http.ListenAndServe(":3000", r)
+}
