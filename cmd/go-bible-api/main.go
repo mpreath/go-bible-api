d52dd7dde2f3faaba5db6736eaa3f6d6161eba0d
Matt Reath <mattreath@icloud.com>
2024-04-04 19:26:37 -0500

    adding viper and base config

--- cmd/go-bible-api/main.go
+++ cmd/go-bible-api/main.go
@@ -1,15 +1,7 @@
 package main
 
-import (
-	"net/http"
-
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
-)
-
 func main() {
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-	r.Get("/", Root)
-	http.ListenAndServe(":3000", r)
+	config := LoadConfig()
+	app := NewApp(config)
+	app.Run()
 }
